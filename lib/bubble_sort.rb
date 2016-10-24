02fde5ce08195a9b2077611b7aea8dddde4eca44
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-10-24 18:55:58 +0800

    bubble_sort method finished

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,8 +1,24 @@
 class BubbleSort
 
-	def input(arr)
+	def bubble_sort(arr)
+
+		arr.length.times do
+			x = 0
+			while x < arr.length - 1
+				if arr[x] > arr[x+1]
+					temp = arr[x]
+					arr[x] = arr[x+1]
+					arr[x+1] = temp
+				end
+			x += 1
+			end
+		end
 		return arr
 	end
 
+	def bubble_sort_by(arr)
+		yield(arr[0],arr[1])
+	end
 
 end
+
--- spec/lib/bubble_sort_spec.rb
+++ spec/lib/bubble_sort_spec.rb
@@ -3,10 +3,27 @@ require "bubble_sort"
 
 describe BubbleSort do 
 
+	context "bubble_sort method" do
+		it "accepting arrays [5,2,3] to [2,3,5]" do
+			bs = BubbleSort.new
+			param = bs.bubble_sort([5,2,3])
+			expect(param).to eq [2,3,5]
+		end
 
-	it "has working input method" do
-		bs = BubbleSort.new
-		param = bs.input([1,2,3])
-		expect(param).to eq [1,2,3]
+		it "accepting arrays with similar numbers [5,78,2,5,1,8,1]" do
+			bs = BubbleSort.new
+			param = bs.bubble_sort([5,78,2,5,1,8,1])
+			expect(param).to eq [1,1,2,5,5,8,78]
+		end
+	end
+
+	context "Testing bubble_sort_by method" do
+		it "accepting a block" do 
+			bs = BubbleSort.new
+			param = bs.bubble_sort_by(["hey","hello","hi"]) do |left,right|
+					left.length - right.length
+				end
+			expect(param).to eq ["hi","hey","hello"]
+		end
 	end
 end
